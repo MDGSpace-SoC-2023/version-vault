9e60ce02751cc9d519d39882249194d06f502197
Kraken57 <kasaad1609@gmail.com>
2023-12-19 21:26:49 +0530

    feat: implement responsive sidebar navigation and update route configuration in App.jsx

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { BsFolder, BsGrid1X2 } from "react-icons/bs";
 
 const Layout = () => {
+  const { pathname } = useLocation();
   const [show, setShow] = useState(false);
 
   return (
@@ -63,6 +66,69 @@ const Layout = () => {
                 </ul>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex mt-16">
+        <div className="sidebar w-[300px] p-5 h-[calc(100vh-70px)] fixed">
+          <div className="px-2 py-2 flex justify-start gap-5 items-center mb-3">
+            <img
+              className="w-[40px] h-[40px] rounded-full"
+              src="https://avatars.githubusercontent.com/u/130333838?s=96&v=4"
+              alt="sidebar image"
+            />
+            <div className="flex justify-center flex-col items-start">
+              <span className="text-[#e0dddd] font-bold text-md">
+                Ahmad Saad
+              </span>
+              <span className="text-[#c4c0c0] text-sm">Free</span>
+            </div>
+          </div>
+          <ul className="px-4 flex flex-col gap-2">
+            <li>
+              <Link
+                to="/"
+                className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${
+                  pathname === "/" ? "bg-[#ffffff26]" : ""
+                } rounded-[4px]`}
+              >
+                <span className="text-xl">
+                  <FaHome />
+                </span>
+                <span className="font-medium">Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/projects"
+                className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${
+                  pathname === "/projects" ? "bg-[#ffffff26]" : ""
+                } rounded-[4px]`}
+              >
+                <span className="text-xl">
+                  <BsFolder />
+                </span>
+                <span className="font-medium">Projects</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/templates"
+                className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${
+                  pathname === "/templates" ? "bg-[#ffffff26]" : ""
+                } rounded-[4px]`}
+              >
+                <span className="text-xl">
+                  <BsGrid1X2 />
+                </span>
+                <span className="font-medium">Templates</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="ml-[300px] w-[calc(100%-300px)]">
+          <div className="py-4 pr-4">
+            <Outlet />
           </div>
         </div>
       </div>
